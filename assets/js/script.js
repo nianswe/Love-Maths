5a6f1d42b330a7027dc0fd4c28031521dda7fc1a
Niklas Andersson <nianswe@hotmail.com>
2024-02-17 11:36:37 +0000

    Add code to calculate two random numbers for the game question

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,23 +10,37 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                rungame(gameType);
             
             }
 
         })
     }
 
+    rungame("addition");
+
+
 })
 
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the users'sanswer has been prosseced
+ */
+function runGame(gameType) {
 
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 
-
-function rungame() {
-    let num1 = Math.floor(Math.random() * 25);
-    let num2 = Math.floor(Math.random() * 25);
 }
+
 
 function checkAnswer() {
 
@@ -44,7 +58,10 @@ function incremantWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textcontent = operand1;
+    document.getElementById('operand2').textcontent = operand2;
+    document.getElementById('operator').textcontent = "+";
 
 }
 
